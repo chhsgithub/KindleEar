e43d11c4cd25a55b2a95058e1b78719d361a6ffa
chhsgithub <chhsemail@gmail.com>
2016-06-12 10:36:50 +0200

    Create yichangtext.py

---
+++ books/yichangtext.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from base import BaseFeedBook
+
+def getBook():
+    return yichang
+
+class yichang(BaseurlBook):
+    title                 = u'异常'
+    description           = u'远。'
+    language              = 'zh-cn'
+    feed_encoding         = "utf-8"
+    page_encoding         = "utf-8"
+    coverfile = "cv_yichang.jpg"
+    oldest_article        = 1
+    feeds = [
+            (u'正文', 'http://feed43.com/5454041806035373.xml')
+           ]
